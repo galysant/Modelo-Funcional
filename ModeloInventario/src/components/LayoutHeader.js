b281e4b9e2339d43d70f55fb4d5d87d16027dfa3
Santiago Andres Galeano Eslava <sgaleanoeslava@gmail.com>
2025-05-26 22:36:47 -0500

    Cambio menor

--- ModeloInventario/src/components/LayoutHeader.js
+++ ModeloInventario/src/components/LayoutHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LayoutHeader = ({ user, onLogout }) => {
   return (
     <header className="bg-white shadow-md py-4 px-6 flex justify-between items-center">
-      <h1 className="text-xl font-bold">SuperStock</h1>
+      <h1 className="text-xl font-bold">StockEasy</h1>
       {user && (
         <div className="flex items-center">
           <span className="mr-4">Hola, {user.username} ({user.role})</span>
